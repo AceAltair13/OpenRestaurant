374a0628aa51cb1b741cf651cc52f066ab81bc24
rushilshah23 <61428940+rushilshah23@users.noreply.github.com>
2021-09-07 01:44:47 +0530

    sort by distance done

--- app/src/main/java/com/example/openrestaurant/RestaurantDataGPS.kt
+++ app/src/main/java/com/example/openrestaurant/RestaurantDataGPS.kt
@@ -2,18 +2,19 @@ package com.example.openrestaurant
 
 import com.google.firebase.firestore.GeoPoint
 
-data class RestaurantDataGPS (
+data class RestaurantDataGPS(
     var name: String? = "",
     var location: GeoPoint? = null,
-    var menu: List<Menu>? = null
+    var menu: List<Menu>? = null,
+    var distance: Int? = 0
 )
 
-data class Menu (
+data class Menu(
     var category: String? = "",
     var items: List<Item>? = null
 )
 
-data class Item (
+data class Item(
     var itemName: String? = "",
     var itemPrice: Int? = 0,
     var image: String? = ""
